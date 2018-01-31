679906c3b079b82c92fe6fa57bf32efca78bc0bf
Dave Edelhart <dave@wonderlandlabs.com>
2018-01-31 12:54:55 -0800

    creating shape based structure

---
+++ test/IsoTriangle.spec.js
@@ -0,0 +1,39 @@
+/*global describe, it*/
+'use strict';
+const expect = require('chai').expect;
+const _ = require('lodash');
+const THREE = require('three');
+
+const IsoTriangle = require('./../lib/IsoTriangle');
+const Shape = require('./../lib/Shape');
+
+describe('IsoTriangle', () => {
+  let root, triangle;
+
+  beforeEach(() => {
+    root = new Shape();
+    root.putPoint(0, 0, 10);
+    root.putPoint(10, 0, 0);
+    root.putPoint(10, 0, 10);
+    triangle = new IsoTriangle(root, 0, [0, 1, 2]);
+  });
+
+  it('.rootPoint', () => {
+    expect(triangle.rootPoint.x).to.eql(0);
+    expect(triangle.rootPoint.y).to.eql(0);
+    expect(triangle.rootPoint.z).to.eql(10);
+  });
+
+  it('.rowPoint', () => {
+    expect(triangle.rowPoint.x).to.eql(10);
+    expect(triangle.rowPoint.y).to.eql(0);
+    expect(triangle.rowPoint.z).to.eql(0);
+  });
+
+  it('.colPoint', () => {
+    expect(triangle.colPoint.x).to.eql(10);
+    expect(triangle.colPoint.y).to.eql(0);
+    expect(triangle.colPoint.z).to.eql(10);
+  });
+
+});
---
+++ test/Shape.spec.js
@@ -0,0 +1,83 @@
+/*global describe, it*/
+'use strict';
+const Shape = require('./../lib/Shape');
+const expect = require('chai').expect;
+const _ = require('lodash');
+const THREE = require('three');
+
+describe('Shape', () => {
+
+  describe('.constructor', () => {
+    let parent, child;
+    const parentIndexes = [2, 1];
+
+    beforeEach(() => {
+      parent = new Shape();
+      parent.putPoint(0, 1, 0);
+      parent.putPoint(1, 0, 1);
+      parent.putPoint(0, 0, 1);
+      child = new Shape(parent, 0, parentIndexes);
+    });
+
+    it('should reflect parent\'s points', () => {
+      let firstPoint = child.getPoint(0);
+      expect(firstPoint.x).to.equal(0);
+      expect(firstPoint.y).to.equal(0);
+      expect(firstPoint.z).to.equal(1);
+    });
+
+    it('should have the same pointIndexes as passed in', () => {
+      expect(child.parentPointIndexes).to.eql(parentIndexes);
+    });
+
+  });
+
+  describe('.putPoint', () => {
+    let shape;
+
+    beforeEach(() => {
+      shape = new Shape();
+    });
+
+    it('should put a point without an index', () => {
+      shape.putPoint(3, 4, 5);
+      let firstPoint = shape.getPoint(0);
+      expect(firstPoint.x).to.equal(3);
+      expect(firstPoint.y).to.equal(4);
+      expect(firstPoint.z).to.equal(5);
+    });
+
+    it('should put a point without an index', () => {
+      shape.putPoint(3, 4, 5, 2);
+      let pointAt2 = shape.getPoint(2);
+      expect(pointAt2.x).to.equal(3);
+      expect(pointAt2.y).to.equal(4);
+      expect(pointAt2.z).to.equal(5);
+    });
+  });
+
+  describe('.putVector', () => {
+    let shape;
+
+    beforeEach(() => {
+      shape = new Shape();
+    });
+
+    it('should put a point without an index', () => {
+      shape.putVector(new THREE.Vector3(3, 4, 5));
+      let firstPoint = shape.getPoint(0);
+      expect(firstPoint.x).to.equal(3);
+      expect(firstPoint.y).to.equal(4);
+      expect(firstPoint.z).to.equal(5);
+    });
+
+    it('should put a point without an index', () => {
+      shape.putVector(new THREE.Vector3(3, 4, 5), 2);
+      let pointAt2 = shape.getPoint(2);
+      expect(pointAt2.x).to.equal(3);
+      expect(pointAt2.y).to.equal(4);
+      expect(pointAt2.z).to.equal(5);
+    });
+  });
+
+});
